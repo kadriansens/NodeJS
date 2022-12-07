0e73f09888b404b301b90d8dbef8906e484bac1b
kadriansens <kadriansens@gmail.com>
2022-12-07 13:50:12 +0100

    First push

---
+++ 2022/Day1/index.js
@@ -0,0 +1,15 @@
+// https://adventofcode.com/2022/day/1
+
+const fs = require('fs');
+
+const calories = fs.readFileSync('text.txt', 'utf8').trimEnd();
+// console.log(calories);
+
+function sol(calories) {
+  const elf = calories
+    .split('\n\n')
+
+  console.log(elf)
+}
+
+sol(calories)
